5b28889c74966e4434e9e23865f0a20ef2c4c3cb
erdos7 <erdos7@iit.uni-miskolc.hu>
2018-03-12 16:57:36 +0100

    2gyak5

---
+++ gyak0205/src/gy0205.java
@@ -0,0 +1,39 @@
+
+public class gy0205 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=1; i<=100; i++){
+			for (int j=1; j<=100; j++){
+				if(isPrime(i)&& isPrime(j) &&(i-2==j)){
+					TwinPrimwNumber++;
+				}
+				
+			}
+		}
+		
+		return true
+		
+	}
+	
+	/* int i;
+	for(i=3; i<=99; i++){
+		if (i =! (i mod 2=0)){
+			
+		
+		}
+		
+		
+	} */
+	
+	
+	
+	
+	
+
+}
