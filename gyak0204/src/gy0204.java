90b5f619c14ced95d0f2c6c4b4edce899cf6fba6
erdos7 <erdos7@iit.uni-miskolc.hu>
2018-03-12 16:57:04 +0100

    2gyak4

---
+++ gyak0204/src/gy0204.java
@@ -0,0 +1,28 @@
+
+public class gy0204 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+}
+
+	
+	public static void buildTree(int num){
+		int i;
+		int j;
+		for ( i=1; i<=5; i++){
+			for (j=1; j<=5; j++){
+				System.out.print(i);
+				
+				
+			}
+			System.out.println(" ");
+		}
+		
+	}
+}
